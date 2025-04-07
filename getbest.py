8c5377faf26579d1da12f0680731d92065f87d86
LinkyNzuza <linky.nzuza07@gmail.com>
2025-04-07 15:45:16 +0200

    Codeupdate

--- getbest.py
+++ getbest.py
@@ -13,12 +13,13 @@ def getCols(f):
 
 def findTop(f,num_col, mark_col):
     ''' finds the top student in the class '''
-    best = best_idx =  0
+    
     for line in f:
         data = line.strip().split(",")
         mark = int(data[mark_col])
+        best = best_idx = max(int(data[mark_col])
         if mark > best:
-            best=mark
+            best = mark
     return best_idx, best
 
 f = open(sys.argv[1])
