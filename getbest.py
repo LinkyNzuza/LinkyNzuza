f44f4984b818a5d8814a51cffbaf160ded5438c1
LinkyNzuza <linky.nzuza07@gmail.com>
2025-04-07 15:54:26 +0200

    Updatecode

--- getbest.py
+++ getbest.py
@@ -2,6 +2,10 @@
 
 import sys
 
+#!/usr/bin/env python3
+
+import sys
+
 def getCols(f):
     ''' Identify the columns that contain the marks and student numbers '''
     headings = f.readline().strip().split(",")
@@ -13,13 +17,33 @@ def getCols(f):
 
 def findTop(f,num_col, mark_col):
     ''' finds the top student in the class '''
-    
+    best = best_idx =  0
     for line in f:
         data = line.strip().split(",")
         mark = int(data[mark_col])
-        best = best_idx = max(int(data[mark_col])
         if mark > best:
-            best = mark
+            best=mark
+    return best_idx, best
+
+f = open(sys.argv[1])
+num_col, mark_col = getCols(f)
+best_idx, best = findTop(f,num_col,mark_col)
+print("The top student was student %s with %d"%(best_idx,best))def getCols(f):
+    ''' Identify the columns that contain the marks and student numbers '''
+    headings = f.readline().strip().split(",")
+    i=1
+    for head in headings:
+        if head == "Student Number": num_col=i
+        elif head == "Mark" : mark_col = i
+    return (num_col, mark_col)
+
+def findTop(f,num_col, mark_col):
+    ''' finds the top student in the class '''
+    best = best_idx = max(int(data[mark_col])
+    for line in f:
+        data = line.strip().split(",")
+        mark = int(data[mark_col])
+        if mark > best:best = mark
     return best_idx, best
 
 f = open(sys.argv[1])
